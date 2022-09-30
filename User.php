0c3bce10cf4722aa1a3674e1af57e69208de7583
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-09-30 11:39:36 +0200

    demo inheritance

--- User.php
+++ User.php
@@ -1,11 +1,16 @@
 <?php
 
-class User
-{
-    private $username = null;
+class User {
     private $email = null;
     private $password = null;
 
+    public function login() {
+        // login user
+    }
+}
+
+class Visitor extends User
+{
     public function setEmail($emailParam) {
         if(is_string($emailParam)) {
             $this->email = $emailParam;
@@ -13,13 +18,13 @@ class User
             throw new Error("email must be of type string");
         }
     }
-
-    public function direBonjour() {
-        echo "salut tout le monde";
-    }
 }
 
-$user1 = new User();
-$user1->setEmail(123);
+class Admin extends User {
+}
 
-var_dump($user1);
+$visitor = new Visitor();
+$admin = new Admin();
+
+var_dump($visitor);
+var_dump($admin);
