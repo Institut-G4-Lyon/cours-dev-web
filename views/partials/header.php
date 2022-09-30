d1d44a33c5cc9aa4205d7ac01d91f30f7b976623
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-09-30 15:33:42 +0200

    add articles link in header

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,7 @@
             <li class="nav-item"><a href="/" class="nav-link <?= $pathname === "/" ? "active" : "" ?>">Home</a></li>
             <li class="nav-item"><a href="/about" class="nav-link <?= str_starts_with($pathname, "/about") ? "active" : "" ?>">A propos</a></li>
             <li class="nav-item"><a href="/search" class="nav-link <?= str_starts_with($pathname, "/search") ? "active" : "" ?>">Recherche</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+            <li class="nav-item"><a href="/articles" class="nav-link <?= str_starts_with($pathname, "/articles") ?>">Articles</a></li>
             <li class="nav-item"><a href="#" class="nav-link">About</a></li>
         </ul>
     </header>
